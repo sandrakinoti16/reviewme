de24104d26fd81defe8c95e5e123e9ff124d34c2
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-06-12 21:21:01 +0300

    make migrations for profile model + update requirement.txt

--- reviewdeck/models.py
+++ reviewdeck/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from pyuploadcare.dj.models import ImageField
+# from pyuploadcare.dj.models import ImageField
 import datetime as dt
 
 

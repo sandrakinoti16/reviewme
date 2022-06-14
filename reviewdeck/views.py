2e24da89b4aa8db8791dfaab6ed61428d308a074
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-06-14 09:51:41 +0300

    implement serializer in views.py

--- reviewdeck/views.py
+++ reviewdeck/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm, RatingsForm
 from rest_framework import viewsets
 from .models import Profile, Post, Rating
-from .serializers import ProfileSerializer, UserSerializer, PostSerializer
+from .serializer import ProfileSerializer, UserSerializer, PostSerializer
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
